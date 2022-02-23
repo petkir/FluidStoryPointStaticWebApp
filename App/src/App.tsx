dfd05d556560b392184e6007ccb8699fa937f305
Peter Paul Kirschner <peter@kirschner.zone>
2022-02-23 21:46:55 +0100

    typo

--- App/src/App.tsx
+++ App/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
       <div className="Header">
         <div>
-          <h1>ADCD 2022 Fluid Story Pocker Sample</h1>
+          <h1>ADCD 2022 Fluid Story Poker Sample</h1>
         </div>
 
         <User />

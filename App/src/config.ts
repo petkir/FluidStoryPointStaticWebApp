acf26332013b84e8f9c77690aae60b7022650b0e
Peter Paul Kirschner <peter@kirschner.zone>
2023-06-19 08:24:30 +0200

    Upgrade fluid 1.1.1

--- App/src/config.ts
+++ App/src/config.ts
@@ -1,4 +1,4 @@
-import { AzureClientProps, AzureFunctionTokenProvider, LOCAL_MODE_TENANT_ID } from "@fluidframework/azure-client";
+import { AzureClientProps, AzureFunctionTokenProvider } from "@fluidframework/azure-client";
 import { InsecureTokenProvider } from "@fluidframework/test-client-utils";
 
 export const useAzure = false;
@@ -10,34 +10,36 @@ export function getConnectionConfig(userId: string, userName: string): AzureClie
     if (useAzure) {
         return ({
             connection: {
-                tenantId: "02af75bb-9f70-487b-9d84-5390f4079a1f",
                 tokenProvider: new AzureFunctionTokenProvider("/api/GetToken", { userId: userId, userName: userName }),
-                orderer: "https://alfred.westeurope.fluidrelay.azure.com",
-                storage: "https://historian.westeurope.fluidrelay.azure.com",
+                tenantId: "02af75bb-9f70-487b-9d84-5390f4079a1f",
+                endpoint: "https://eu.fluidrelay.azure.com",
+                type: "remote",
             }
         })
     }
+    
     if (!useAzure) {
         //http://localhost:7071
         return ({
             connection: {
-                tenantId: "02af75bb-9f70-487b-9d84-5390f4079a1f",
-                tokenProvider: new AzureFunctionTokenProvider("https://lemon-plant-01c5e7c10.1.azurestaticapps.net/api/GetToken", { userId: userId, userName: userName }),
-                orderer: "https://alfred.westeurope.fluidrelay.azure.com",
-                storage: "https://historian.westeurope.fluidrelay.azure.com",
-            }
+                type: "remote",
+                tokenProvider: new AzureFunctionTokenProvider(
+                  "https://lemon-plant-01c5e7c10.1.azurestaticapps.net/api/GetToken", { userId: userId, userName: userName }
+                ),
+               tenantId: "02af75bb-9f70-487b-9d84-5390f4079a1f",
+                endpoint: "https://eu.fluidrelay.azure.com"
+              }
         })
     }
-   
+
     return ({
         connection: {
-            tenantId: LOCAL_MODE_TENANT_ID,
+            type: "local",
             tokenProvider: new InsecureTokenProvider("fooBar", {
                 id:userId,
                 
             }),
-            orderer: "http://localhost:7070",
-            storage: "http://localhost:7070",
+            endpoint: "http://localhost:7070"
         }
     });
 } 
